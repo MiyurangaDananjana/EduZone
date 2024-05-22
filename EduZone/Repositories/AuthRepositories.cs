8245fcf247efc013504a28d932013837f05f866b
Dananjana Ananda <dananjanaA@icbtcampus.edu.lk>
2024-05-22 10:03:01 +0530

    `Added Product model and AuthRepositories with DB connection`
     Added two new files to the EduZone.csproj: Product.cs and AuthRepositories.cs, and removed the Models folder. Modified the Web.config file to include a connectionStrings section for SQL Server database connection. Created a new Product class with Id, Name (required), and Price properties. Also added a new AuthRepositories class with a GetProducts method to retrieve product list from the database using the DefaultConnection defined in the Web.config file.

---
+++ EduZone/Repositories/AuthRepositories.cs
@@ -0,0 +1,47 @@
+﻿using EduZone.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace EduZone.Repositories
+{
+    public class AuthRepositories
+    {
+        private readonly string _connectionString;
+
+        public AuthRepositories(string connectionString)
+        {
+            _connectionString = _connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            var products = new List<Product>();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var query = "SELECT Id, Name, Price FROM Products";
+
+                using (var command = new SqlCommand(query, connection))
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        products.Add(new Product
+                        {
+                            Id = (int)reader["Id"],
+                            Name = reader["Name"].ToString(),
+                            Price = (decimal)reader["Price"]
+                        });
+                    }
+                }
+            }
+
+            return products;
+        }
+    }
+}
