4f6c3cecb633149c468e328580590c71cab7e006
Dhananjana Miyuranga <83136587+MiyurangaDananjana@users.noreply.github.com>
2024-09-11 13:14:34 +0530

    change db connection

--- EduZone/Repositories/DbConnection.cs
+++ EduZone/Repositories/DbConnection.cs
@@ -7,7 +7,8 @@ namespace EduZone.Repositories
     public class DbConnection
     {
         // Hardcoded connection string
-        private string connectionString = "Data Source=HQ-IT-PC36\\SQLEXPRESS;Initial Catalog=EduZone;Integrated Security=True;Encrypt=False";
+        private string connectionString = "Data Source=HQ-SDU-PC02\\SQLEXPRESS;Initial Catalog=EduZone;User ID=sa;Password=abc;Encrypt=False";
+
 
         public DataTable ExecuteQuery(string sqlQuery, SqlParameter[] parameters = null)
         {
